651a341fb64d27c6003b927e3e4711dcb6d121d7
viktor-enzell <viktor.enzell@gmail.com>
2019-02-10 16:50:49 +0100

    Add comment parameters.

--- game_engine/node.py
+++ game_engine/node.py
@@ -16,31 +16,61 @@ class Node:
         self.has_won = False
 
     def add_child(self, child):
+        """
+        :param child:
+        """
         self.children.append(child)
 
     def get_children(self):
+        """
+        :return:
+        """
         return self.children
 
     def get_string(self):
+        """
+        :return:
+        """
         return "My turn: " + str(self.is_my_turn) + "'s turn. Leaf: " + str(self.is_leaf)
 
     def is_leaf(self):
+        """
+        :return:
+        """
         return self.is_leaf
 
     def set_is_leaf(self):
+        """
+        :return:
+        """
         self.is_leaf = True
 
     def is_my_turn(self):
+        """
+        :return:
+        """
         return self.is_my_turn
 
     def set_has_won(self):
+        """
+        :return:
+        """
         self.has_won = True
 
     def has_won(self):
+        """
+        :return:
+        """
         return self.has_won
 
     def get_cell(self):
+        """
+        :return:
+        """
         return self.cell
 
     def get_passed_piece(self):
+        """
+        :return:
+        """
         return self.passed_piece
