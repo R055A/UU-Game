bad2258ba5893750cde1602b4d43d9553ae14488
Adam Ross <14985050+R055A@users.noreply.github.com>
2019-02-17 18:38:22 +0100

    change PlayerEasyAI class file name
    Co-Authored-By: pallekan <pallekan@users.noreply.github.com>
    Co-Authored-By: sravoryk-fork <sravoryk-fork@users.noreply.github.com>

--- game_engine/player_ai_easy.py
+++ game_engine/player_ai_easy.py
@@ -4,6 +4,8 @@ from game_engine.player_ai import PlayerAI
 class PlayerEasyAI(PlayerAI):
     """
     The easy difficulty AI class
+    Author(s): Pelle Ingvast, Maxime Gaide, Adam Ross
+    Last-edit-date: 17/02/19
     """
 
     def __init__(self, game, name):
@@ -19,13 +21,14 @@ class PlayerEasyAI(PlayerAI):
         Method for easy difficulty AI piece choosing
         :return: the selected piece for placing on the board
         """
-        return self.get_best(dict({str(pce): self.hard(self.game.pieces[pce])
-                                   for pce in self.game.pieces}.items()), True)
+        return self.get_best(dict({str(pce): self.select_best_sim_count(self.
+                hard(self.game.pieces[pce]), True, self.game.pieces[pce]) for
+                pce in self.game.pieces}.items()), True)
 
-    def place_piece(self, selected_piece):
+    def place_piece(self, pce):
         """
         Method for easy difficulty AI piece placing on board
-        :param selected_piece the piece selected for placing on board
+        :param pce the piece selected for placing on board
         """
-        y, x = self.easy(selected_piece)[0]
-        self.game.board[y][x] = selected_piece
+        y, x = self.select_best_sim_count(self.easy(pce), False, pce)[0]
+        self.game.board[y][x] = pce
