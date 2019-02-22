3ffac00a2c4ad50191536f3de7933a4f5b6a4469
Adam Ross <14985050+R055A@users.noreply.github.com>
2019-02-22 21:19:56 +0100

    Add test for sixteen pieces initialized to GamePlatform

--- test/test_game_platform_gui.py
+++ test/test_game_platform_gui.py
@@ -20,4 +20,33 @@ class TestGamePlatform(TestCase):
         self.assertTrue(isinstance(test, GamePlatform))
         test.update()
         sleep(15)
+        test.quit()
         test.destroy()
+
+    def test_pieces_created(self):
+        """
+        Tests sixteen unique game piece created at initialization
+        """
+        test = GamePlatform(Tk(), Play())
+        self.assertTrue(len(test.pieces) == 16 and len(test.pieces) * 2
+                        == len(test.canvas.find_all()))
+        self.assertTrue(test.pieces[i].colour == 'red' and test.canvas.type(i)
+                        == 'rectangle' for i in range(8))
+        self.assertTrue(test.pieces[i].colour == 'gray1' for i in range(8, 16))
+        self.assertTrue(test.canvas.type(9) == 'oval')
+        self.assertTrue(test.canvas.type(10) == 'oval')
+        self.assertTrue(test.canvas.type(11) == 'rectangle')
+        self.assertTrue(test.canvas.type(12) == 'oval')
+        self.assertTrue(test.canvas.type(13) == 'rectangle')
+        self.assertTrue(test.canvas.type(14) == 'oval')
+        self.assertTrue(test.canvas.type(15) == 'rectangle')
+        self.assertTrue(test.canvas.type(16) == 'rectangle')
+        self.assertTrue(test.canvas.type(i) == 'rectangle' for i in range(16, 24))
+        self.assertTrue(test.canvas.type(25) == 'oval')
+        self.assertTrue(test.canvas.type(26) == 'oval')
+        self.assertTrue(test.canvas.type(27) == 'rectangle')
+        self.assertTrue(test.canvas.type(28) == 'oval')
+        self.assertTrue(test.canvas.type(29) == 'rectangle')
+        self.assertTrue(test.canvas.type(30) == 'oval')
+        self.assertTrue(test.canvas.type(31) == 'rectangle')
+        self.assertTrue(test.canvas.type(32) == 'rectangle')
