dacd5ca1e4ab6db16de477a582250e05b149561e
LaurinKerle <l8amdkhc>
2019-02-24 10:31:40 +0100

    Added Board to GUI

---
+++ test/test_board.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from game_platform.board import Board
+from tkinter import Canvas
+
+SIZE = 800  # the height and width of the test GUI canvas
+
+
+class TestBoard(TestCase):
+    """
+    Tests the Board class
+    Author(s): Laurin Kerle
+    Last-edit-date: 24/02/2019
+    """
+
+    def test_board_class(self):
+        """
+        Tests board class instance
+        """
+        test = Board(Canvas(height=SIZE, width=SIZE), 10 , 10, 10)
+        self.assertTrue(isinstance(test, Board))
+
+
+
