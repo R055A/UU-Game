9a6169298a81379658791fe88873363e5efe008c
Adam Ross <14985050+R055A@users.noreply.github.com>
2019-03-04 11:09:49 +0100

    Merge

--- game_platform_gui/game_platform.py
+++ game_platform_gui/game_platform.py
@@ -2,13 +2,6 @@ from tkinter import Frame, Canvas
 from game_platform_gui.piece import Piece
 from game_platform_gui.board import Board
 
-TITLE = 'UU-Game'
-HEIGHT = 800  # test size
-WIDTH = 800  # test size
-BACKGROUND_COLOUR = 'white'
-SLCTD_PCE_X_POS = 500
-SLCTD_PCE_Y_POS = 50
-
 
 class GamePlatform(Frame):
     """
@@ -16,6 +9,13 @@ class GamePlatform(Frame):
     Author(s): Adam Ross
     Last-edit-date: 21/02/2019
     """
+
+    TITLE = 'UU-Game'
+    HEIGHT = 800
+    WIDTH = 800
+    COL = 'white'  # background colour
+    SLCTD_PCE_X_POS = 500
+    SLCTD_PCE_Y_POS = 50
 
     def __init__(self, master, play):
         """
@@ -26,8 +26,8 @@ class GamePlatform(Frame):
         super().__init__(master)
         self.master = master
         self.play = play
-        self.master.title(TITLE)
-        self.canvas = Canvas(height=HEIGHT, width=WIDTH, bg=BACKGROUND_COLOUR)
+        self.master.title(self.TITLE)
+        self.canvas = Canvas(height=self.HEIGHT, width=self.WIDTH, bg=self.COL)
         self.canvas.pack()
         self.pieces = dict({i: Piece(self.canvas, self.play.game.pieces[i])
                             for i in self.play.game.pieces}.items())
@@ -48,4 +48,5 @@ class GamePlatform(Frame):
             self.canvas.delete(self.pieces[str(pce_id - 1)].vertical)
         self.pieces[str(pce_id - 1)] = Piece(self.canvas, bin(pce_id - 1)[2:].
                                              zfill(self.play.game.N),
-                                             SLCTD_PCE_X_POS, SLCTD_PCE_Y_POS)
+                                             self.SLCTD_PCE_X_POS,
+                                             self.SLCTD_PCE_Y_POS)
